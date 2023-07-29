05f27c841b0f1b66b3f0ce9345403dabda6e5d3c
LapisAquamarin <tinbeo113@gmail.com>
2023-07-30 00:00:15 +0700

    fix

--- app/src/main/java/com/example/healthwise_project/AccountFragment.java
+++ app/src/main/java/com/example/healthwise_project/AccountFragment.java
@@ -55,6 +55,7 @@ public class AccountFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
