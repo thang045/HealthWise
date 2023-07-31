9b77f8c8b2b31041b9de67405759a8f5a170e2a5
kevin <thangnguyen0405@gmail.com>
2023-07-31 20:57:56 +0700

    completely done for appointment class

--- app/src/main/java/com/example/healthwise_project/Appointment.java
+++ app/src/main/java/com/example/healthwise_project/Appointment.java
@@ -8,4 +8,57 @@ public class Appointment {
     int id;
     String name, phone, sympton;
     Date datetime;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSympton() {
+        return sympton;
+    }
+
+    public void setSympton(String sympton) {
+        this.sympton = sympton;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public Appointment(int id, String name, String phone, String sympton, Date datetime) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.sympton = sympton;
+        this.datetime = datetime;
+    }
+
+    public Appointment()
+    {
+
+    }
+
 }
