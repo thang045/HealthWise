e5f8097a248ee7162f14342efaedeae647130c1c
August268 <122625689+August268@users.noreply.github.com>
2023-08-01 14:33:49 +0700

    Load doctors' name and department from firebase through dropdown spinner

--- app/src/main/java/com/example/healthwise_project/AppointmentFragment.java
+++ app/src/main/java/com/example/healthwise_project/AppointmentFragment.java
@@ -5,6 +5,7 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -13,9 +14,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
@@ -25,10 +34,15 @@ import java.util.Calendar;
  */
 public class AppointmentFragment extends Fragment {
 
+    ArrayList<String> doctorNameArrayList = new ArrayList<>(), departmentArrayList = new ArrayList<>();
     Calendar date;
     Button btnDateTimePicker;
-
+    Spinner spnDropdown_Appointment;
     TextView tvDateTime;
+
+    CustomSpinnerAdapter adapter;
+
+    ArrayList<Doctor> doctorArrayList = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -79,7 +93,9 @@ public class AppointmentFragment extends Fragment {
 
         btnDateTimePicker = (Button) view.findViewById(R.id.btnDateTimePicker);
         tvDateTime = (TextView) view.findViewById(R.id.tvDateTime);
+        spnDropdown_Appointment = (Spinner) view.findViewById(R.id.spnDropdown_Appointment);
 
+        loadDataFromFirebase();
 
         btnDateTimePicker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -181,6 +197,7 @@ public class AppointmentFragment extends Fragment {
                                 break;
                         }
 
+                        // AM and PM for time
                         switch (date.get(Calendar.AM_PM)){
                             case 0:
                                 AM_PM = "am";
@@ -200,5 +217,43 @@ public class AppointmentFragment extends Fragment {
                 }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), false).show();
             }
         }, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DATE)).show();
+    }
+    public void loadDataFromFirebase(){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://healthwise-project" +
+                        "-default-rtdb.asia-southeast1.firebasedatabase.app");
+        DatabaseReference myRef = database.getReference("Doctors");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Doctor doctor = dataSnapshot.getValue(Doctor.class);
+                    doctorArrayList.add(doctor);
+                }
+                for (Doctor d : doctorArrayList){
+                    doctorNameArrayList.add(d.getName());
+                    departmentArrayList.add(d.getDepartment());
+                }
+
+                Log.d("Doctors", doctorNameArrayList.get(0));
+                Log.d("Departments", departmentArrayList.get(0));
+
+                String[] doctorName = new String[doctorNameArrayList.size()],
+                        department = new String[departmentArrayList.size()];
+
+                doctorName = doctorNameArrayList.toArray(doctorName);
+                department = departmentArrayList.toArray(department);
+
+                adapter = new CustomSpinnerAdapter(getContext(),  doctorName,
+                        department);
+                spnDropdown_Appointment.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
