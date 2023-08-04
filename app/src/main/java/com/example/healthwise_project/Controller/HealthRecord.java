7c47fd89c3be7f78e1aaeaf43efe09b7afea2fce
kevin <thangnguyen0405@gmail.com>
2023-08-04 17:03:55 +0700

    showing all healthrecord records

--- app/src/main/java/com/example/healthwise_project/Controller/HealthRecord.java
+++ app/src/main/java/com/example/healthwise_project/Controller/HealthRecord.java
@@ -41,6 +41,8 @@ public class HealthRecord extends AppCompatActivity {
         reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                healthRecordList = new ArrayList<HealthRecordClass>();
+
                 for (DataSnapshot dataSnapshot : snapshot.getChildren())
                 {
                     if(dataSnapshot.child("idUser").getValue().toString().equals(userID))
@@ -52,10 +54,13 @@ public class HealthRecord extends AppCompatActivity {
 //                        System.out.println(datetime);
 //                        System.out.println(symptoms);
 //                        System.out.println(data.getDate());
-                        healthRecordList = new ArrayList<HealthRecordClass>();
                         healthRecordList.add(data);
 
                     }
+                }
+                for (HealthRecordClass health: healthRecordList)
+                {
+                    System.out.println(health.getSymtomps());
                 }
                 System.out.println(healthRecordList.get(0).toString());
 
